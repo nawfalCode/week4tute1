a7b56f11e7e316e8a87e927ae01bcd55b4cb0dcb
Nawfal Ali <nawfalrezo@gmail.com>
2019-08-19 12:44:45 +1000

    add delete and listAll

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let express=require('express');
+let express = require('express');
 
-let app=express();
+let app = express();
 
-let db=[];
+let db = [];
 
 // localhost:8080/addNo/80    : to add a new nymber to the db
 // localhost:8080/deleteNo/80  : to delete a number // the first occ
@@ -10,18 +10,51 @@ let db=[];
 // localhost:8080/listAll    : to get all the number and their ids
 
 
-app.get('/addNo/:number',function(req,res){
-        let theId=getNewRandomId();
-        let obj={id:theId,no:parseInt(req.params.number)};
-        console.log(obj);
-        db.push(obj);
-        res.send("Thank you!!!");        
+app.get("/deleteNo/:number2Delete", function (req, res) {
+
+    let found = false;
+    for (let i = 0; i < db.length && !found; i++) {
+        if (db[i].no === parseInt(req.params.number2Delete)) {
+            db.splice(i, 1);
+            found = true;
+        }
+    }
+
+    let msg = "";
+    if (found) {
+        msg = "We deleted your number!!!";
+    } else {
+        msg = "Sorry!! we could not find it!!"
+    }
+    res.send(msg);
+
+});
+
+app.get('/addNo/:number', function (req, res) {
+    let theId = getNewRandomId();
+    let obj = {
+        id: theId,
+        no: parseInt(req.params.number)
+    };
+    console.log(obj);
+    db.push(obj);
+    res.send("Thank you!!!");
 });
 
 
-function getNewRandomId(){
+app.get("/listAll", function (req, res) {
+
+    let st = "";
+    for (let i = 0; i < db.length; i++) {
+        st += (i) + " - " + db[i].id + " | " + db[i].no + "<br>";
+    }
+
+    res.send(st);
+});
+
+function getNewRandomId() {
     let id;
-    id=Math.round(Math.random()*1000);
+    id = Math.round(Math.random() * 1000);
     return id;
 }
 
